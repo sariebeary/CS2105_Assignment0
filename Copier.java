608f1fce4068222eda00cc17bfd619a5b4ed1ae7
Sarah Bednar <shb66@pitt.edu>
2018-01-31 18:19:58 +0800

    finished copier

--- Copier.java
+++ Copier.java
@@ -1,7 +1,6 @@
 
 import java.io.*;
 
-
 /**
  * CS 2105
  *
@@ -20,6 +19,26 @@ public class Copier {
             System.out.println("Error: File " + input + " does not exist.");
             System.exit(1);
         }
+        byte[] buffer = new byte[1024];
+        FileInputStream fis = new FileInputStream(args[0]);
+        BufferedInputStream bis = new BufferedInputStream(fis);
+
+        FileOutputStream fos = new FileOutputStream(args[1]);
+        BufferedOutputStream bos = new BufferedOutputStream(fos);
+        try {
+            try {
+                int numBytes;
+                while ((numBytes = bis.read(buffer)) != -1) {
+                    bos.write(buffer, 0, numBytes);
+                }
+            } finally {
+                bos.close();
+            }
+        } finally {
+            bis.close();
+        }
+
+        System.out.println(args[0] + " is successfully copied to " + args[1]);
 
     }
 
