0c551a602337b000f806ab325e3e1ba37470d269
Sarah Bednar <shb66@pitt.edu>
2018-01-31 17:27:58 +0800

    finished ipaddress

--- IPAddress.java
+++ IPAddress.java
@@ -1,9 +1,6 @@
 
-import java.util.*;
-import java.io.*;
-
 /**
- * CS 2105 
+ * CS 2105
  *
  * @author Sarah Bednar
  */
@@ -12,12 +9,24 @@ public class IPAddress {
     public static void main(String[] args) {
 
         if (args.length != 1) {
-            System.out.println("Error: Enter only one argument, the IP address.");
+            System.out.println("Error: Enter only one argument, the binary representation of the ip address.");
             System.exit(1);
         }
-
+        System.out.println(getDecimalAddress(args[0]));
     }
 
-
+    public static String getDecimalAddress(String binaryIp) {
+        try {
+            long ip = Long.parseLong(binaryIp,2);
+            return ((ip >> 24) & 0xFF)
+                    + "." + ((ip >> 16) & 0xFF)
+                    + "." + ((ip >> 8) & 0xFF)
+                    + "." + (ip & 0xFF);
+        } catch (NumberFormatException ex) {
+            System.out.println("Error: Must enter binary integer value");
+            System.exit(1);
+        }
+        return null;
+    }
 
 }
