a65404b93ba0e0f5ae6156a83d14846b39d0a8d6
Sarah Bednar <shb66@pitt.edu>
2018-01-31 14:20:58 +0800

    Initial commit

---
+++ Checksum.java
@@ -0,0 +1,29 @@
+
+import java.util.*;
+import java.io.*;
+
+/**
+ * CS 2105 
+ *
+ * @author Sarah Bednar
+ */
+public class Checksum {
+
+    public static void main(String[] args) {
+
+        if (args.length != 1) {
+            System.out.println("Error: Enter only one argument, the file to read.");
+            System.exit(1);
+        }
+
+        File input = new File(args[0]);
+        if (!isReadableFile(input)) {
+            System.out.println("Error: File " + input + " does not exist.");
+            System.exit(1);
+        }
+
+    }
+
+
+
+}
---
+++ IPAddress.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+import java.io.*;
+
+/**
+ * CS 2105 
+ *
+ * @author Sarah Bednar
+ */
+public class IPAddress {
+
+    public static void main(String[] args) {
+
+        if (args.length != 1) {
+            System.out.println("Error: Enter only one argument, the IP address.");
+            System.exit(1);
+        }
+
+    }
+
+
+
+}
