2e290a1b8eef538b50405ebfc3379488a81852d2
Sarah Bednar <shb66@pitt.edu>
2018-01-31 17:51:49 +0800

    Finished checksum

--- Checksum.java
+++ Checksum.java
@@ -1,29 +1,32 @@
 
-import java.util.*;
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.zip.CRC32;
 
 /**
- * CS 2105 
+ * CS 2105
  *
  * @author Sarah Bednar
  */
 public class Checksum {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         if (args.length != 1) {
             System.out.println("Error: Enter only one argument, the file to read.");
             System.exit(1);
         }
-
         File input = new File(args[0]);
-        if (!isReadableFile(input)) {
+        if (!input.canRead() || !input.isFile()) {
             System.out.println("Error: File " + input + " does not exist.");
             System.exit(1);
         }
-
+        byte[] bytes = Files.readAllBytes(Paths.get(args[0]));
+        CRC32 crc = new CRC32();
+        crc.update(bytes); // compute checksum
+        System.out.println(crc.getValue()); // print checksum
     }
-
-
 
 }
--- Copier.java
+++ Copier.java
@@ -1,29 +1,26 @@
 
-import java.util.*;
 import java.io.*;
 
+
 /**
- * CS 2105 
+ * CS 2105
  *
  * @author Sarah Bednar
  */
 public class Copier {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        if (args.length != 1) {
-            System.out.println("Error: Enter only one argument, the file to read.");
+        if (args.length != 2) {
+            System.out.println("Usage: java Copier [src] [dest]");
             System.exit(1);
         }
-
         File input = new File(args[0]);
-        if (!isReadableFile(input)) {
+        if (!input.canRead() || !input.isFile()) {
             System.out.println("Error: File " + input + " does not exist.");
             System.exit(1);
         }
 
     }
-
-
 
 }
